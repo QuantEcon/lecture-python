44c08ae60822a312f8db1acb73de09254ae63d16
Matt McKay <mamckay@gmail.com>
2020-03-28 16:17:36 +1100

    update travis to cater for complete file deletions

--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -1,10 +1,19 @@
 echo "PR: $TRAVIS_PULL_REQUEST"
 echo "COMMIT RANGE: $TRAVIS_COMMIT_RANGE"
 CHANGED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep '\.rst' | tr '\n' ' ')
-echo "List of Changed Files: $CHANGED_FILES"
-if [ -z "$CHANGED_FILES" ]; then
+#Check for Full Deletions
+SPHINX_FILES=""
+for f in $CHANGED_FILES
+do
+    if [ -f $f ]
+    then
+        SPHINX_FILES="$SPHINX_FILES $f"
+    fi
+done
+echo "List of Changed Files: $SPHINX_FILES"
+if [ -z "$SPHINX_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
-    make coverage FILES="$CHANGED_FILES"
-    make linkcheck FILES="$CHANGED_FILES"
+    make coverage FILES="$SPHINX_FILES"
+    make linkcheck FILES="$SPHINX_FILES"
 fi
