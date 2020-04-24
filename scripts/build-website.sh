f6c44974cc284e333655885a2a4351d84346051e
Matt McKay <mamckay@gmail.com>
2020-04-24 13:23:10 +1000

    reorder export command

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -12,11 +12,11 @@ do
 done
 echo "List of Changed RST Files: $RST_FILES"
 if [ -z "$RST_FILES" ]; then
-    echo "No RST Files have changed -- nothing to do in this PR"
     export BUILD_NETLIFY=false
+    echo "No RST Files have changed -- nothing to do in this PR"
 else
+    export BUILD_NETLIFY=true
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
     make website THEMEPATH=theme/lecture-python-intro.theme FILES="$RST_FILES"
-    ls _build/website/jupyter_html/*
-    export BUILD_NETLIFY=true
+    ls _build/website/jupyter_html/*  #Ensure build files are created
 fi
