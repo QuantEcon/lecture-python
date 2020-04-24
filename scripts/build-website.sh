cab675d89e42217ad229c84634cf758e4408302c
Matt McKay <mamckay@gmail.com>
2020-04-24 11:10:11 +1000

    add customisation for lecture-python-intro

---
+++ scripts/build-website.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MODIFIED_FILES="$1"
+
+RST_FILES=""
+for F in $MODIFIED_FILES
+do
+    if [[ $F == *.rst ]]
+    then
+        RST_FILES="$RST_FILES $F"
+    fi
+done
+echo "List of Changed RST Files: $RST_FILES"
+if [ -z "$RST_FILES" ]; then
+    echo "No RST Files have changed -- nothing to do in this PR"
+else
+    RST_FILES="$RST_FILES source/rst/index_toc.rst"
+    make website THEMEPATH=theme/lecture-python-intro.theme FILES="$RST_FILES"
+    ls _build/website/jupyter_html/*
+fi
