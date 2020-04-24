3b2d81b49125c5988a65090c55dc4c72165dab70
mmcky <mmcky@users.noreply.github.com>
2020-04-24 13:33:35 +1000

    Add GitHub Actions for execution, link checking and previews (#131)
    * add github actions scripts to enable execution, link checking and html previews
    
    * add customisation for lecture-python-intro
    
    * set variable so netlify doesn't deploy if no RST files have changed
    
    * update from environmental variable
    
    * use set-env
    
    * try new if syntax for env var
    
    * change order
    
    * reorder export command
    
    * remove travis

---
+++ scripts/build-website.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MODIFIED_FILES="$1"
+
+RST_FILES=""
+for F in $MODIFIED_FILES
+do
+    if [[ $F == *.rst ]]
+    then
+        RST_FILES="$RST_FILES $F"
+    fi
+done
+echo "List of Changed RST Files: $RST_FILES"
+if [ -z "$RST_FILES" ]; then
+    export BUILD_NETLIFY=false
+    echo "No RST Files have changed -- nothing to do in this PR"
+else
+    export BUILD_NETLIFY=true
+    RST_FILES="$RST_FILES source/rst/index_toc.rst"
+    make website THEMEPATH=theme/lecture-python-intro.theme FILES="$RST_FILES"
+    ls _build/website/jupyter_html/*  #Ensure build files are created
+fi
