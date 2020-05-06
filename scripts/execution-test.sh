63949aea53d1cb68e6e05901f357fac245108522
Matt McKay <mamckay@gmail.com>
2020-05-06 16:02:27 +1000

    revert changes to execution-scripts

--- scripts/execution-test.sh
+++ scripts/execution-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CLEAN_BUILD=False
 MODIFIED_FILES="$1"
 
 RST_FILES=""
@@ -9,17 +8,10 @@ do
     if [[ $F == *.rst ]]
     then
         RST_FILES="$RST_FILES $F"
-    elif [[ $F == environment.yml ]]
-    then
-        CLEAN_BUILD=True
     fi
 done
-
 echo "List of Changed RST Files: $RST_FILES"
-echo "Clean Build Requested: $CLEAN_BUILD"
-if [ $CLEAN_BUILD ]; then
-    make coverage
-elif [ -z "$RST_FILES" ]; then
+if [ -z "$RST_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
