28f5ab253c518ed7bb9f9a21323b5d96178addfa
Matt McKay <mamckay@gmail.com>
2020-05-06 13:26:49 +1000

    add clean build request when environment.yml changes

--- scripts/execution-test.sh
+++ scripts/execution-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CLEAN_BUILD=False
 MODIFIED_FILES="$1"
 
 RST_FILES=""
@@ -8,10 +9,17 @@ do
     if [[ $F == *.rst ]]
     then
         RST_FILES="$RST_FILES $F"
+    elif [[ $F == environment.yml ]]
+    then
+        CLEAN_BUILD=True
     fi
 done
+
 echo "List of Changed RST Files: $RST_FILES"
-if [ -z "$RST_FILES" ]; then
+echo "Clean Build Requested: $CLEAN_BUILD"
+if [[ CLEAN_BUILD == True]]; then
+    make coverage
+elif [ -z "$RST_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
