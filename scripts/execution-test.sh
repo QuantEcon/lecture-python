a68d4a8a346b0d8223480c5c657b2ddc179f8284
mmcky <mmcky@users.noreply.github.com>
2020-05-06 19:13:13 +1000

    [Github Actions] Full build for changes to environment.yml (#181)
    * add update to ga execution testing to issue complete build for changes in environment.yml
    
    * adjust filters for file types
    
    * add list of conda environment

--- scripts/execution-test.sh
+++ scripts/execution-test.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+CLEAN_BUILD=False
 MODIFIED_FILES="$1"
 
 RST_FILES=""
 for F in $MODIFIED_FILES
 do
+    if [[ $F == environment.yml ]]
+    then
+        CLEAN_BUILD=True
+        break
+    fi
+    #Extract List of RST Files
     if [[ $F == *.rst ]]
     then
         RST_FILES="$RST_FILES $F"
     fi
 done
+
 echo "List of Changed RST Files: $RST_FILES"
-if [ -z "$RST_FILES" ]; then
+echo "Clean Build Requested: $CLEAN_BUILD"
+if [ $CLEAN_BUILD ]; then
+    make coverage
+elif [ -z "$RST_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
