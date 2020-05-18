4c1337ebef91640170c9e3f1e6c7aea48d4414a5
Matt McKay <mamckay@gmail.com>
2020-05-18 11:33:07 +1000

    add support for fork PRs to build preview with public theme

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 MODIFIED_FILES="$1"
+PRIVATE_THEME=true
 
+# Find List of RST Files
 RST_FILES=""
 for F in $MODIFIED_FILES
 do
@@ -17,6 +19,12 @@ if [ -z "$RST_FILES" ]; then
 else
     echo "::set-env name=BUILD_NETLIFY::true"
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
-    make website THEMEPATH=theme/lecture-python.theme FILES="$RST_FILES"
+    if [ "$PRIVATE_THEME" = true]; then
+        echo "Running Build with PRIVATE theme"
+        make website THEMEPATH=theme/lecture-python.theme FILES="$RST_FILES"
+    else
+        echo "Running Build with PUBLIC theme"
+        make website FILES="$RST_FILES"
+    fi
     ls _build/website/jupyter_html/*  #Ensure build files are created
 fi
