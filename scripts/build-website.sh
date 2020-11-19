7d20a5c66f9c03b12a6a14795befddfe42a0e717
mmcky <mamckay@gmail.com>
2020-11-19 13:20:12 +1100

    remove set-env due to deprecation

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -15,10 +15,10 @@ done
 echo "List of Changed RST Files: $RST_FILES"
 echo "Building with Private theme: $PRIVATE_THEME"
 if [ -z "$RST_FILES" ]; then
-    echo "::set-env name=BUILD_NETLIFY::false"
+    echo "BUILD_NETLIFY=false" >> $GITHUB_ENV
     echo "No RST Files have changed -- nothing to do in this PR"
 else
-    echo "::set-env name=BUILD_NETLIFY::true"
+    echo "BUILD_NETLIFY=true" >> $GITHUB_ENV
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
     if [ "$PRIVATE_THEME" = true ]; then
         make website THEMEPATH=theme/lecture-python.theme FILES="$RST_FILES"
